638b237ea431a585d4c28fc2cb4e178d6cd73d7d
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2025-04-14 17:10:41 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,15 @@
+﻿namespace Labb_2___Threads
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public int Speed { get; set; }
+
+        public Car(string brand, int speed)
+        {
+            Brand = brand;
+            Speed = speed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Labb_2___Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Toyota = new Car("Toyota", 120);
+            Car Volvo = new Car("Volvo", 120);
+            Car Lamborghini = new Car("Lamborghini", 120);
+
+         
+    }
+}
